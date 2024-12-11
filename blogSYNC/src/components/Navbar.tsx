ed94ddca35255af629ea8f0fc5676ba1d2a0b372
Shaan Singh <ss5432@outlook.com>
2024-12-11 20:03:35 +0530

    Added inline styling.

--- blogSYNC/src/components/Navbar.tsx
+++ blogSYNC/src/components/Navbar.tsx
@@ -4,7 +4,16 @@ const Navbar = () => {
       <h1>blogSYNC</h1>
       <div className="links">
         <a href="/">home</a>
-        <a href="/create">newBlog</a>
+        <a
+          href="/create"
+          style={{
+            color: "white",
+            backgroundColor: "#f1356d",
+            borderRadius: "8px",
+          }}
+        >
+          newBlog
+        </a>
       </div>
     </nav>
   );
