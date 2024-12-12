c6af90dc9b31257a36f1a9418c3b92f4543c5cc1
Shaan Singh <ss5432@outlook.com>
2024-12-12 18:32:09 +0530

    Having fun with useState.

--- blogSYNC/src/components/Home.tsx
+++ blogSYNC/src/components/Home.tsx
@@ -1,14 +1,20 @@
+import { useState } from "react";
+
 const HomePage = () => {
+  const [name, setName] = useState("mario");
+  const [age, setAge] = useState(25);
+
+  const handleClick = () => {
+    setName((prev) => (prev == "mario" ? "luigi" : "mario"));
+    setAge((prev) => (prev == 25 ? 30 : 25));
+  };
   return (
     <div className="homepage">
       <h2>Home</h2>
-      <button
-        onClick={() => {
-          console.log("hi");
-        }}
-      >
-        Hi
-      </button>
+      <p>
+        {name} is {age}
+      </p>
+      <button onClick={handleClick}>Hi</button>
     </div>
   );
 };
