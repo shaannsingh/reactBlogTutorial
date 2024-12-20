982949724d8f70cf589a58d768163417f8bb1cd2
Shaan Singh <ss5432@outlook.com>
2024-12-21 00:57:06 +0530

    Catching errors has never been easier.

--- blogSYNC/src/components/Home.tsx
+++ blogSYNC/src/components/Home.tsx
@@ -5,6 +5,7 @@ import { Blog } from "../types";
 const HomePage = () => {
   const [blogs, setBlogs] = useState<Blog[]>([]);
   const [isPending, setIsPending] = useState<boolean>(true);
+  const [error, setError] = useState(null);
 
   const handleDelete = (id: number) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
@@ -15,20 +16,24 @@ const HomePage = () => {
     setTimeout(() => {
       fetch("http://localhost:8000/blogs")
         .then((response) => {
+          if (!response.ok) throw Error("Couldn't fetch");
           return response.json();
         })
         .then((data) => {
           setBlogs(data);
           setIsPending(false);
+          setError(null);
         })
         .catch((err) => {
-          console.log(err.message);
+          setError(err.message);
+          setIsPending(false);
         });
     }, 1000);
   }, []);
 
   return (
     <div className="homepage">
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {blogs && (
         <BlogList blogs={blogs} heading="All" handleDelete={handleDelete} />
