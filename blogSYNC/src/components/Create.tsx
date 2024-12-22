1a07e4bd4801ba3c79b163379b17ad3cf9363066
Shaan Singh <ss5432@outlook.com>
2024-12-22 16:35:55 +0530

    Added controlled input forms to make sure the form state renders with the change.

--- blogSYNC/src/components/Create.tsx
+++ blogSYNC/src/components/Create.tsx
@@ -1,7 +1,40 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState<string>("");
+  const [body, setBody] = useState<string>("");
+  const [author, setAuthor] = useState<string>("");
+
   return (
     <div className="create">
       <h2>Create a New Blog</h2>
+      <form>
+        <label>Blog Title:</label>
+        <input
+          placeholder="Add your title here"
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog Body:</label>
+        <textarea
+          placeholder="Write your blog here"
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        />
+        <label>Blog author:</label>
+        <select
+          title="Author name"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        >
+          <option value="Mario">Mario</option>
+          <option value="Luigi">Luigi</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
     </div>
   );
 };
