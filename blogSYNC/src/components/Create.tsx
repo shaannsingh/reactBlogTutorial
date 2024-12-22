1014ff25030c3b6ce54120cfda9f5f9b0d536cc5
Shaan Singh <ss5432@outlook.com>
2024-12-22 16:53:52 +0530

    Sending post requests to add blogs to JSON server and then display on homepage (since we're fectching from the same endpoint that we're inputting to.)

--- blogSYNC/src/components/Create.tsx
+++ blogSYNC/src/components/Create.tsx
@@ -3,12 +3,21 @@ import React, { useState } from "react";
 const Create = () => {
   const [title, setTitle] = useState<string>("");
   const [body, setBody] = useState<string>("");
-  const [author, setAuthor] = useState<string>("");
+  const [author, setAuthor] = useState<string>("Mario");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const blog = { title, body, author };
-    console.log(blog);
+    setIsLoading(true);
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("New blog added.");
+      setIsLoading(false);
+    });
   };
 
   return (
@@ -39,7 +48,8 @@ const Create = () => {
           <option value="Mario">Mario</option>
           <option value="Luigi">Luigi</option>
         </select>
-        <button>Add Blog</button>
+        {!isLoading && <button>Add Blog</button>}
+        {isLoading && <button disabled>Adding blog...</button>}
       </form>
     </div>
   );
